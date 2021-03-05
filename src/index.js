97f9b0e6b1e1543a454f4e7d628131d38b1208fc
Diana Schegoleva <dschegoleva@gmail.com>
2021-03-05 17:26:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let brackets of bracketsConfig) {
+  	var joined = brackets.join('');
+    
+  	if (str == '') return true;
+    else if (str.indexOf(joined) !== -1) {
+    	return check(deletePart(str, joined), bracketsConfig);
+    }
+  }
+  return false;
+}
+
+function deletePart(str, joined) {
+	let arr = str.split('');
+  arr.splice(str.indexOf(joined), joined.length);
+	return arr.join('');
 }
